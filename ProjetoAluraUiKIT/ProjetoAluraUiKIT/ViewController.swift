ba56e6be555627689496ec6df1df0a8a086f7c91
MichellSantoos <60993267+michelldossantos@users.noreply.github.com>
2024-02-20 09:23:27 -0300

    Projeto base

---
+++ ProjetoAluraUiKIT/ProjetoAluraUiKIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjetoAluraUiKIT
+//
+//  Created by Michel Santos on 20/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
