1bb9299776713d451b80ba6f287b95328a8a2dc3
MichellSantoos <60993267+michelldossantos@users.noreply.github.com>
2024-02-23 15:14:58 -0300

    Create project SwiftUI

---
+++ ViagemAluraSwifUI/ViagemAluraSwifUI/ViagemAluraSwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViagemAluraSwifUIApp.swift
+//  ViagemAluraSwifUI
+//
+//  Created by Michel Santos on 23/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ViagemAluraSwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
