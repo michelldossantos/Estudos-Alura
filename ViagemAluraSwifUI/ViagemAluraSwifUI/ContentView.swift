608033866278bf67cf53691169cb29e9e4c223b3
MichellSantoos <60993267+michelldossantos@users.noreply.github.com>
2024-02-27 10:18:43 -0300

    adjusting spacing

--- ViagemAluraSwifUI/ViagemAluraSwifUI/ContentView.swift
+++ ViagemAluraSwifUI/ViagemAluraSwifUI/ContentView.swift
@@ -9,18 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Viagens")
-            Text("ESPECIAL")
-            Text("BRASIL")
+        GeometryReader { view in
             
-            List {
-                Text("Viagens")
-                Text("ESPECIAL")
-                Text("BRASIL")
+            VStack {
+                VStack {
+                    Text("Viagens").foregroundColor(.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .lineLimit(0)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding(.top, 10
+                        )
+                    Text("ESPECIAL").foregroundColor(.white)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                    Text("BRASIL").foregroundColor(.white)
+                        .font(.custom("Avenir Black", size: 23))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                    
+                }
+                .frame(width: view.size.width, height: 180, alignment: .top)
+                .background(Color.purple)
+                List {
+                    Text("Viagens")
+                    Text("ESPECIAL")
+                    Text("BRASIL")
+                }
             }
         }
-        .padding()
     }
 }
 
