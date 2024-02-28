cb82b42616286b09f3c0424dbbea6a2bf43afb61
MichellSantoos <60993267+michelldossantos@users.noreply.github.com>
2024-02-28 00:10:16 -0300

    types of devices

--- ViagemAluraSwifUI/ViagemAluraSwifUI/Views/ContentView.swift
+++ ViagemAluraSwifUI/ViagemAluraSwifUI/Views/ContentView.swift
@@ -13,12 +13,12 @@ struct ContentView: View {
     var body: some View {
         GeometryReader { view in
             VStack(spacing: 0) {
-                HeaderView().frame(width: view.size.width, height: 200, alignment: .top)
+                HeaderView().frame(width: view.size.width, height: self.horizontalSizeClass == .compact ? 200 : 300, alignment: .top)
 
                 List(viagens) { viagem in
                     TravelViewCell(viagem: viagem)
                 } .listStyle(.inset)
-                    .offset(y: self.horizontalSizeClass == .compact ? -25 : 30)
+                    .offset(y: self.horizontalSizeClass == .compact ? -25 : -60)
             }
         }
     }
@@ -31,11 +31,27 @@ struct ContentView_Previews: PreviewProvider {
             .previewDisplayName("iPhone 14")
 
         ContentView()
-            .previewDevice(PreviewDevice(rawValue: "iPad Pro"))
-            .previewDisplayName("iPad")
-        
-        ContentView()
-            .previewDevice(PreviewDevice(rawValue: "iPhone SE (3rd generation)"))
-            .previewDisplayName("Se 3")
+            .previewDevice(PreviewDevice(rawValue: "iPad Pro (9.7-inch)"))
+            .previewDisplayName("iPad 9.7")
     }
 }
+
+
+//ContentView()
+//    .previewDevice(PreviewDevice(rawValue: "iPhone 14"))
+//    .previewDisplayName("iPhone 14")
+//ContentView()
+//    .previewDevice(PreviewDevice(rawValue: "iPad mini (6th generation)"))
+//    .previewDisplayName("iPad Mini")
+//
+//ContentView()
+//    .previewDevice(PreviewDevice(rawValue: "iPad Pro (9.7-inch)"))
+//    .previewDisplayName("iPad 9.7")
+//
+//ContentView()
+//    .previewDevice(PreviewDevice(rawValue: "iPad Pro (12.9-inch) (6th generation)"))
+//    .previewDisplayName("iPad 12.9")
+//
+//ContentView()
+//    .previewDevice(PreviewDevice(rawValue: "iPhone SE (3rd generation)"))
+//    .previewDisplayName("Se 3")
