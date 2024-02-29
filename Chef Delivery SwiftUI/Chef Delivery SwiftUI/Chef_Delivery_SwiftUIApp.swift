228ce5a91ac70e2306054c5ca22180dcca11a55d
MichellSantoos <60993267+michelldossantos@users.noreply.github.com>
2024-02-28 22:06:42 -0300

    init project Chef Delivery

---
+++ Chef Delivery SwiftUI/Chef Delivery SwiftUI/Chef_Delivery_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chef_Delivery_SwiftUIApp.swift
+//  Chef Delivery SwiftUI
+//
+//  Created by Michel Santos on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Chef_Delivery_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
