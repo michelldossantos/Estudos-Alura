cfd0d9107c3915db12f0f0db60bd532a24f40afc
MichellSantoos <60993267+michelldossantos@users.noreply.github.com>
2024-03-05 22:48:20 -0300

    adjusting lazy grid

--- Chef Delivery SwiftUI/Chef Delivery SwiftUI/GridView/OrderTypeGrid.swift
+++ Chef Delivery SwiftUI/Chef Delivery SwiftUI/GridView/OrderTypeGrid.swift
@@ -11,11 +11,14 @@ struct OrderTypeGrid: View {
     let orders = ["Mercado", "Bar", "Descontos", "Gourmet", "Pet", "Bebidas", "Lojas", "Test", "Test3", "Test4", "Test5" ]
     
     var body: some View {
-        LazyHGrid(rows: [GridItem(.fixed(100)),
-                         GridItem(.fixed(100))]) {
+        LazyHGrid(rows: [
+            GridItem(.fixed(100)),
+            GridItem(.fixed(100))
+        ]) {
             ForEach(orders, id: \.self) { orderItem in
                 Text(orderItem)
             }
+            
         }
     }
 }
