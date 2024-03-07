a4b0c30f0d172c737940522bf827a788b920e972
MichellSantoos <60993267+michelldossantos@users.noreply.github.com>
2024-03-06 23:12:28 -0300

    Add scrool

--- Chef Delivery SwiftUI/Chef Delivery SwiftUI/App/ContentView.swift
+++ Chef Delivery SwiftUI/Chef Delivery SwiftUI/App/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           NavigationBar()
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack {
+                    OrderTypeGrid()
+                }
+            }
         }
         .padding()
     }
