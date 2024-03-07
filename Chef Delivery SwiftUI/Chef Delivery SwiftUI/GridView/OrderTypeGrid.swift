940dff7980614bbf73af126096ec6fe1cf0931b5
MichellSantoos <60993267+michelldossantos@users.noreply.github.com>
2024-03-06 23:06:04 -0300

    adjusting grid

--- Chef Delivery SwiftUI/Chef Delivery SwiftUI/GridView/OrderTypeGrid.swift
+++ Chef Delivery SwiftUI/Chef Delivery SwiftUI/GridView/OrderTypeGrid.swift
@@ -8,21 +8,23 @@
 import SwiftUI
 
 struct OrderTypeGrid: View {
+    var gridLayout: [GridItem] {
+        return Array(repeating: GridItem(.flexible(), spacing: 10), count: 2)
+    }
+    
     var body: some View {
-        LazyHGrid(rows: [
-            GridItem(.fixed(100)),
-            GridItem(.fixed(100))
-        ]) {
+        LazyHGrid(rows: gridLayout, spacing: 15) {
             ForEach(ordersMock) { orderItem in
                 OrderTypeView(orderType: orderItem)
             }
-            
-        }
+        }.frame(height: 200)
+            .padding(.horizontal, 16)
+            .padding(.top, 16)
     }
 }
 
 struct OrderTypeGrid_Previews: PreviewProvider {
     static var previews: some View {
-        OrderTypeGrid()
+        OrderTypeGrid().previewLayout(.sizeThatFits)
     }
 }
