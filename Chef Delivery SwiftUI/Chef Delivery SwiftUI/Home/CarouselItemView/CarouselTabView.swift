ba44454d88f55a3a86a84ea9bf7f559a17db5add
MichellSantoos <michelsantos15@gmail.com>
2024-03-21 07:51:29 -0300

    setup automatic carrousel

--- Chef Delivery SwiftUI/Chef Delivery SwiftUI/Home/CarouselItemView/CarouselTabView.swift
+++ Chef Delivery SwiftUI/Chef Delivery SwiftUI/Home/CarouselItemView/CarouselTabView.swift
@@ -8,25 +8,36 @@
 import SwiftUI
 
 struct CarouselTabView: View {
+    @State private var correntIndex = 1
+    
     let ordersMock: [OrderType] = [
         .init(id: 1, name: "banner burguer", image: "barbecue-banner"),
         .init(id: 2, name: "banner prato feito", image: "brazilian-meal-banner"),
-        .init(id: 3, name: "banner burguer", image: "barbecue-banner")
+        .init(id: 3, name: "banner burguer", image: "picanha")
     ]
     
     var body: some View {
-        TabView {
+        TabView(selection: $correntIndex) {
             ForEach(ordersMock) { mock in
                 CarouselItemView(order: mock)
+                    .tag(mock.id)
             }
         }.frame(height: 180)
             .tabViewStyle(.page(indexDisplayMode: .always))
+            .onAppear {
+                Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                    if correntIndex > ordersMock.count {
+                        correntIndex = 1
+                    }
+                    correntIndex += 1
+                }
+            }
     }
 }
 
 struct CarouselTabView_Previews: PreviewProvider {
     static var previews: some View {
         CarouselTabView().previewLayout(.sizeThatFits)
-
+        
     }
 }
