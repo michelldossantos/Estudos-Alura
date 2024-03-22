773aa1d020d10c5afdba20633674f42638b0a710
MichellSantoos <60993267+michelldossantos@users.noreply.github.com>
2024-03-21 23:17:17 -0300

    adjusting CodingKeys

--- Chef Delivery SwiftUI/Chef Delivery SwiftUI/Model/StoreType.swift
+++ Chef Delivery SwiftUI/Chef Delivery SwiftUI/Model/StoreType.swift
@@ -15,4 +15,10 @@ struct StoreType: Decodable, Identifiable {
     let location: String
     let stars: Int
     let products: [ProductType]
+
+    private enum CodingKeys: String, CodingKey {
+        case id, name, location, stars, products
+        case logoImage = "logo_image"
+        case headerImage = "header_image"  
+    }
 }
