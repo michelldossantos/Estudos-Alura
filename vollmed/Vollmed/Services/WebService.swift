ce97218baacfcc8ab5ac38abc67adba61aef569b
MichellSantoos <60993267+michelldossantos@users.noreply.github.com>
2024-03-29 22:01:04 -0300

    create SpecialistEndpoint

--- vollmed/Vollmed/Services/WebService.swift
+++ vollmed/Vollmed/Services/WebService.swift
@@ -12,19 +12,20 @@ enum APIError: Error {
 }
 
 struct WebService {
-    
-    private let baseURL = "http://localhost:3000"
-    
     func  getAllSpecialists() async throws -> Result<[Specialist], APIError> {
-        let endpoint = baseURL + "/especialistas"
-        guard let url = URL(string: endpoint) else {
-            return .failure(.invalidURL)
-        }
+        let url = SpecialistEndpoint.getAllSpecialists()
         
         let (data, _) = try await URLSession.shared.data(from: url)
         let specialists = try JSONDecoder().decode([Specialist].self, from: data)
         
         return .success(specialists)
-        
+    }
+}
+
+struct SpecialistEndpoint {
+    static let baseURL = "http://localhost:3000"
+    
+    static func getAllSpecialists() -> URL {
+        return URL(string: baseURL + "/especialistas")!
     }
 }
