6d6a2799591cab49d22bd091123496ad19508a8b
MichellSantoos <60993267+michelldossantos@users.noreply.github.com>
2024-03-29 22:42:16 -0300

    Create func getImages

--- vollmed/Vollmed/Services/WebService.swift
+++ vollmed/Vollmed/Services/WebService.swift
@@ -5,7 +5,7 @@
 //  Created by Giovanna Moeller on 12/09/23.
 //
 
-import Foundation
+import UIKit
 
 enum APIError: Error {
     case invalidURL
@@ -14,6 +14,27 @@ enum APIError: Error {
 }
 
 struct WebService {
+func getImages(from urlImage: String) async throws -> Result<UIImage?, APIError> {
+    guard let url = URL(string: urlImage) else {
+        return .failure(.invalidURL)
+    }
+    
+    do {
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            return .failure(.requestFailed)
+        }
+        
+        let image = UIImage(data: data)
+        
+        return .success(image)
+    } catch {
+        return .failure(.decodingFailed)
+    }
+}
+
     func getAllSpecialists() async throws -> Result<[Specialist], APIError> {
         let endpoint = SpecialistEndpoint.getAllSpecialists()
         
