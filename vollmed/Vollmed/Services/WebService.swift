ef0204cecfcf00174b42748a9bbb6a9981c37fa3
MichellSantoos <60993267+michelldossantos@users.noreply.github.com>
2024-03-29 21:55:26 -0300

    simple calling service

--- vollmed/Vollmed/Services/WebService.swift
+++ vollmed/Vollmed/Services/WebService.swift
@@ -7,11 +7,24 @@
 
 import Foundation
 
+enum APIError: Error {
+    case invalidURL
+}
+
 struct WebService {
     
     private let baseURL = "http://localhost:3000"
     
-    func getAllSpecialists() {
+    func  getAllSpecialists() async throws -> Result<[Specialist], APIError> {
+        let endpoint = baseURL + "/especialistas"
+        guard let url = URL(string: endpoint) else {
+            return .failure(.invalidURL)
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let specialists = try JSONDecoder().decode([Specialist].self, from: data)
+        
+        return .success(specialists)
         
     }
 }
