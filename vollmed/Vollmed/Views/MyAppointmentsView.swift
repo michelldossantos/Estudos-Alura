f30429a38d96fb8e8caa76445b33eed99003ebce
MichellSantoos <michelsantos15@gmail.com>
2024-04-05 22:58:43 -0300

    creat view appointments

--- vollmed/Vollmed/Views/MyAppointmentsView.swift
+++ vollmed/Vollmed/Views/MyAppointmentsView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct MyAppointmentsView: View {
+    let service = WebService()
+    @State private var appointments: [AppointmentResult] = []
+    
+    func getAllAppointments() async {
+        
+        do {
+            let result = try await service.getAllAppointments(patientID: patientID)
+            
+            switch result {
+            case .success(let appointmets):
+                self.appointments = appointmets
+            case .failure(let error):
+                print("ocorreu um erro \(error)")
+            }
+        } catch {
+            print("ocorreu um erro catch \(error)")
+        }
+    }
+    
     var body: some View {
-        Text("Olaa")
+        ScrollView {
+            ForEach(appointments) { appointment in
+                SpecialistCardView(specialist: appointment.specialist)
+            }
+        }.onAppear {
+            Task {
+               await getAllAppointments()
+            }
+        }
     }
 }
 
