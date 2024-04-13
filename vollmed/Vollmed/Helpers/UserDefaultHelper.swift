ee8ec9054f1a004a34a3bcbe8044c20d972bea3e
MichellSantoos <60993267+michelldossantos@users.noreply.github.com>
2024-04-13 00:01:41 -0300

    UserDefaultKeys

---
+++ vollmed/Vollmed/Helpers/UserDefaultHelper.swift
@@ -0,0 +1,27 @@
+//
+//  UserDefaultHelper.swift
+//  Vollmed
+//
+//  Created by Michel Santos on 12/04/24.
+//
+
+import Foundation
+
+enum UserDefaultKeys: String {
+    case token
+    case patientID
+}
+
+struct UserDefaultHelper {
+    static func save(value: String, key: String) {
+        UserDefaults.standard.setValue(value, forKey: key)
+    }
+    
+    static func getValue(key: String) -> String? {
+        UserDefaults.standard.string(forKey: key)
+    }
+    
+    static func remove(key: String) {
+        UserDefaults.standard.removeObject(forKey: key)
+    }
+}
