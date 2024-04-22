82e74b3183c98cab4814c10195da645bdb91c08a
MichellSantoos <60993267+michelldossantos@users.noreply.github.com>
2024-04-22 09:43:04 -0300

    Start setup HTTPClient

--- vollmed/Vollmed/NetWorking/Base/HTTPClient.swift
+++ vollmed/Vollmed/NetWorking/Base/HTTPClient.swift
@@ -8,5 +8,46 @@
 import Foundation
 
 protocol HTTPClient {
-    func sendRequest<T: Decodable>(endpoint: Endpoint, responseModel: T.Type) async -> Result<T, APIError>
+    func sendRequest<T: Decodable>(endpoint: Endpoint, responseModel: T.Type?) async -> Result<T, APIError>
+}
+
+extension HTTPClient {
+    func sendRequest<T: Decodable>(endpoint: Endpoint, responseModel: T.Type) async -> Result<T, APIError> {
+        var urlComponents = URLComponents()
+        
+        urlComponents.scheme = endpoint.scheme
+        urlComponents.host = endpoint.host
+        urlComponents.path = endpoint.path
+        urlComponents.port = 3000
+        
+        guard let url = urlComponents.url else {
+            return .failure(.invalidURL)
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = endpoint.method.rawValue
+        request.allHTTPHeaderFields = endpoint.header
+        
+        if let body = endpoint.body {
+            request.httpBody =  try? JSONSerialization.data(withJSONObject: body)
+        }
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            guard let respose = response as? HTTPURLResponse  else {
+                return .failure(.noResponse)
+            }
+            
+           
+            switch respose.statusCode {
+            case 200...209:
+                guard let responseModel = responseModel else {
+                    return .success(nil)
+                }
+            }
+            
+        } catch {
+            
+        }
+    }
 }
