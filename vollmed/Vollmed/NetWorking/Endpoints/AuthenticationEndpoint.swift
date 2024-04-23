e218238af7706c62225bbacaa51cc8919e2dacde
MichellSantoos <michelsantos15@gmail.com>
2024-04-23 08:22:20 -0300

    create AuthenticationEndpoint

---
+++ vollmed/Vollmed/NetWorking/Endpoints/AuthenticationEndpoint.swift
@@ -0,0 +1,43 @@
+//
+//  AuthenticationEndpoint.swift
+//  Vollmed
+//
+//  Created by Michel Santos on 23/04/24.
+//
+
+import Foundation
+
+enum AuthenticationEndpoint {
+    case logout
+}
+
+extension AuthenticationEndpoint: Endpoint {
+    var path: String {
+        switch self {
+        case .logout:
+            return "/auth/logout"
+        }
+    }
+    
+    var method: RequestMethod {
+        return .post
+    }
+    
+    var header: [String : String]? {
+        guard let token = AuthenticatorManager.shared.token else {
+            return nil
+        }
+        
+        return [
+            "Authorization": "Bearer \(token)",
+            "Content-Type": "application/json"
+        ]
+    }
+    
+    var body: [String : String]? {
+        switch self {
+        case .logout:
+            return nil
+        }
+    }
+}
