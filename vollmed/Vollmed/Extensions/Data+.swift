b8821598491393adfe33c68870136151d4f15ab3
MichellSantoos <60993267+michelldossantos@users.noreply.github.com>
2024-04-27 13:06:27 -0300

    add extension Data to Dictionary

--- vollmed/Vollmed/Extensions/Data+.swift
+++ vollmed/Vollmed/Extensions/Data+.swift
@@ -11,4 +11,15 @@ extension Data {
     func toString() -> String {
         return String(data: self, encoding: .utf8) ?? ""
     }
+    
+    func convertToDictionary() -> [String: Any] {
+        do {
+            if let dictionary = try JSONSerialization.jsonObject(with: self, options: []) as? [String: Any] {
+                return dictionary
+            }
+        } catch {
+            print("Erro ao converter Data para dicionário: \(error.localizedDescription)")
+        }
+        return [:]
+    }
 }
